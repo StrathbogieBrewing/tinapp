ff33c4d8ab86eb44082e49cf99b21420bb81cc92
Johny Hoops <john@tinbus.org>
2023-09-10 14:41:40 +0000

    add attributes

--- example.c
+++ example.c
@@ -1,19 +1,46 @@
 #include<stdio.h>
 
 #define TINAPP_FEATURE_SELF 0
+#define TINAPP_DATA_MAX_SIZE 8
 
+// All parameters  values are exchanged as strings of upto 8 characters
+// Data types are inferred from format 1.20 (float), 12 (integer), 
+// 12:00:01 or 12:01 (time), 23-09-17 (date), text (enumeration) 
+typedef enum tinapp_const_attribute {
+        TINAPP_ATTRIBUTE_NAME = 0,
+	TINAPP_ATTRIBUTE_UNITS,
+	TINAPP_ATTRIBUTE_MINIMUM,
+	TINAPP_ATTRIBUTE_MAXIMUM,
+	TINAPP_ATTRIBUTE_STEP,
+	TINAPP_CONST_ATTRIBUTE_COUNT 
+} tinapp_const_attribute;
 
-// All parameters and values are exchanged as strings of upto 8 characters
-// Data types are inferred from format 1.20, 12, 12:00:01 or 12:01 (time), 23-09-17 (date)
-typedef enum tinapp_parameter{
-        TINAPP_PARAMETER_NAME = 0,
-	TINAPP_PARAMETER_VALUE,
-	TINAPP_PARAMETER_UNITS,
-	TINAPP_PARAMETER_MAXIMUM,
-	TINAPP_PARAMETER_MINIMUM,
-	TINAPP_PARAMETER_STEP,
-	TINAPP_PARAMETER_COUNT,	
-} tinapp_parameter;
+typedef enum tinapp_attribute {
+	TINAPP_ATTRIBUTE_VALUE = TINAPP_CONST_ATTRIBUTE_COUNT,
+	TINAPP_ATTRIBUTE_ENUMERATE,
+	TINAPP_ATTRIBUTE_COUNT,
+} tinapp_attribute;	
+
+typedef struct tinapp_data {
+	char size;
+	char data[TINAPP_DATA_MAX_SIZE];
+} tinapp_data_t;
+
+typedef struct tinapp_message {
+	char node_id;
+	char feature_id;
+	char parameter_id;
+	tinapp_data_t data;
+} tinapp_message_t;
+
+typedef tinapp_data_t (*tinapp_get_func)();
+typedef void (*tinapp_set_func)(tinapp_data_t value);
+
+typedef struct tinapp_feature {
+	char* attributess[TINAPP_CONST_ATTRIBUTE_COUNT];
+	tinapp_get_func get_func;
+	tinapp_set_func set_func;
+} tinapp_feature_t;
 
 typedef enum {
 	FEATURE_SELF = TINAPP_FEATURE_SELF,
